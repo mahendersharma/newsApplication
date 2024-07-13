3bc24d3a60424f2cd75e0e51ac1d4782baacc551
Mahender Sharma <mahender.sharma@phonato.com>
2024-07-13 12:39:09 +0530

    cloudnary & multer

---
+++ server/app/adminApi/utils/cloudnary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudnary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+    // upload file on cloudnary
+    const res = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file has been upload successfully
+    console.log("File is uploaded cloudnary", res.url);
+    return res
+  } catch (error) {
+    fs.unlinkSync(localFilePath)// remove the loccaly saved temporary file as the upload
+    return null
+  }
+};
+
+
+
+export default {uploadOnCloudnary}
