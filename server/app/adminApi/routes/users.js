a2a1fab9852097cd0b1c3b21ff1aa1eeb4bd264b
Mahender Sharma <mahender.sharma@phonato.com>
2024-07-13 22:00:21 +0530

    complete server create basic project start 1st step advance route

--- server/app/adminApi/routes/users.js
+++ server/app/adminApi/routes/users.js
@@ -2,6 +2,6 @@ import {Router} from 'express'
 import { registerUser } from '../controllers/userController.js'
 const router = Router()
 
-router.route('/register').post(registerUser)
+router.post("/register", registerUser);
 
 export default router
